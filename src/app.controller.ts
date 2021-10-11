ed2fa977ba59c70d9289764b17d00b9642b61a62
Andrushchyk Viktoriia <andrushykv@gmail.com>
2021-10-11 11:10:49 +0300

    main

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Lab1AndrushchukService } from './lab1-andrushchuk/lab1-andrushchuk.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly lab1Service: Lab1AndrushchukService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return this.lab1Service();
   }
 }
